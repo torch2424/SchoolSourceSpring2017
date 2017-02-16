c4bdbe559b863ff1a6f1f3508cc18c04f4b172e1
Aaron Turner <aaron@aaronthedev.com>
2017-02-16 14:16:59 -0800

    Finished Stats project, and got messges sending for distributed computing

--- Distributed Computing - CECS327/assignment2 - distributedchat/Chat.java
+++ Distributed Computing - CECS327/assignment2 - distributedchat/Chat.java
@@ -1,6 +1,9 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.lang.*;
+import org.json.simple.*;
+
 /*****************************//**
  * \brief It implements a distributed chat.
  * It creates a ring and delivers messages
@@ -8,180 +11,212 @@ import java.util.*;
  **********************************/
 public class Chat {
 
-  /*
-     Json Messages:
+/*
+   Json Messages:
 
-     {
-          "type" :  "JOIN",
-          "parameters" :
-                 {
-                      "myAlias" : string,
-                      "myPort"  : number
-                 }
-     }
+   {
+        "type" :  "JOIN",
+        "parameters" :
+               {
+                    "myAlias" : string,
+                    "myPort"  : number
+               }
+   }
 
-     {
-          "type" :  "ACCEPT",
-          "parameters" :
-                 {
-                     "ipPred"    : string,
-                     "portPred"  : number
-                 }
-      }
-
-      {
-           "type" :  "LEAVE",
-           "parameters" :
-           {
-               "ipPred"    : string,
-               "portPred"  : number
-           }
-      }
-
-     {
-           "type" :  "Put",
-          "parameters" :
-           {
-               "aliasSender"    : string,
-               "aliasReceiver"  : string,
-               "message"        : string
-          }
-     }
-
-     {
-          "type" :  "NEWSUCCESSOR",
-          "parameters" :
-          {
-              "ipSuccessor"    : string,
-              "portSuccessor"  : number
-          }
-     }
-   */
-
-  // My info
-  public String alias;
-  public int myPort;
-  // Successor
-  public String ipSuccessor;
-  public int portSuccessor;
-  // Predecessor
-  public String ipPredecessor;
-  public int portPredecessor;
-
-
-
-  /*****************************//**
-   * \class Server class "chat.java"
-   * \brief It implements the server
-   **********************************/
-  private class Server implements Runnable
-  {
-    public Server()
-    {
+   {
+        "type" :  "ACCEPT",
+        "parameters" :
+               {
+                   "ipPred"    : string,
+                   "portPred"  : number
+               }
     }
-    /*****************************//**
-     * \brief It allows the system to interact with the participants.
-     **********************************/
-    public void run() {
+
+    {
+         "type" :  "LEAVE",
+         "parameters" :
+         {
+             "ipPred"    : string,
+             "portPred"  : number
+         }
+    }
+
+   {
+         "type" :  "Put",
+        "parameters" :
+         {
+             "aliasSender"    : string,
+             "aliasReceiver"  : string,
+             "message"        : string
+        }
+   }
+
+   {
+        "type" :  "NEWSUCCESSOR",
+        "parameters" :
+        {
+            "ipSuccessor"    : string,
+            "portSuccessor"  : number
+        }
+   }
+ */
+
+// My info
+public String alias;
+public int myPort;
+// Successor
+public String ipSuccessor;
+public int portSuccessor;
+// Predecessor
+public String ipPredecessor;
+public int portPredecessor;
+
+//Debug logging
+public static boolean debugMode;
+public static void debugLog(String log) {
+  if(debugMode) {
+    System.out.println(log);
+  }
+}
+
+
+
+/*****************************//**
+ * \class Server class "chat.java"
+ * \brief It implements the server
+ **********************************/
+private class Server implements Runnable
+{
+  public Server()
+  {
+  }
+/*****************************//**
+ * \brief It allows the system to interact with the participants.
+ **********************************/
+public void run() {
+        try {
+                Chat.debugLog("Server, myPort: " + myPort);
+                ServerSocket servSock = new ServerSocket(myPort);
+                boolean serverRunning = true;
+                while (serverRunning)
+                {
+                  try {
+                  // Get client connections
+                  Socket clntSock = servSock.accept();
+                  //Create a new thread to handle the connection
+                  ObjectInputStream ois = new ObjectInputStream(clntSock.getInputStream());
+                  ObjectOutputStream oos = new ObjectOutputStream(clntSock.getOutputStream());
+                  //reads the message using JsonParser and handle the messages
+                  String message = (String) ois.readObject();
+
+                  System.out.println("Got Message! " + message);
+
+                  //only if the message requires a response
+                  //oos.write(m);
+
+                  //Close a client socket on every read/write
+                  clntSock.close();
+
+                  //Sleep for the next connection
+                  Thread.sleep(1000);
+                  } catch(Exception e) {
+                    System.out.println(e);
+                  }
+                }
+        } catch (Exception e)
+        {
+                // Handle the exception
+        }
+}
+}
+
+
+
+/*****************************//*
+ * \brief It implements the client
+ **********************************/
+private class Client implements Runnable
+{
+
+public Client()
+{
+}
+/*****************************//**
+ * \brief It allows the user to interact with the system.
+ **********************************/
+public void run()
+{
+
+    try {
+    // The first thing to do is to join
+    // ask the ip and port when joining and set ipSuccessor = ip, portSuccessor = port
+    //Socket socket = new Socket(ipSuccessor, portSuccessor);
+    boolean clientRunning = true;
+        while (clientRunning)
+        {
             try {
-                    ServerSocket servSock = new ServerSocket(myPort);
-                    while (true)
-                    {
-                            /*
-                               Socket clntSock = servSock.accept(); // Get client connections
-                               Create a new thread to handle the connection
+              // Reastablish connection with server continuosly
+              Socket serverSocket = new Socket("localhost", 2424);
+              ObjectOutputStream oos = new ObjectOutputStream(serverSocket.getOutputStream());
+              ObjectInputStream ois = new ObjectInputStream(serverSocket.getInputStream());
 
-                               ObjectInputStream  ois = new
-                               ObjectInputStream(clntSock.getInputStream());
-                               ObjectOutputStream oos = new
-                               ObjectOutputStream(clntSock.getOutputStream());
-                               ois.read();    reads the message using JsonParser and handle the messages
+              // Create the mssages m using JsonWriter and send it as stream
+              JSONObject paramObject = new JSONObject();
+              paramObject.put("myAlias", alias);
+              paramObject.put("myPort", myPort);
+              JSONObject joinObject = new JSONObject();
+              joinObject.put("type", "JOIN");
+              joinObject.put("parameters", paramObject);
 
-                               oos.write(m);   only if the message requires a response
-                               clntSock.close();
-                             */
-                    }
-            } catch (IOException e)
-            {
-                    // Handle the exception
+              //this sends the message
+              StringWriter jsonMessage = new StringWriter();
+              joinObject.writeJSONString(jsonMessage);
+              String stringMessage = jsonMessage.toString();
+              oos.writeObject(stringMessage);
+              Thread.sleep(1000);
+            } catch(Exception e) {
+              System.out.println(e);
             }
-    }
-  }
+        }
+      } catch(Exception e) {
+        System.out.println(e);
+      }
+}
+}
 
+/*****************************//**
+ * Starts the threads with the client and server:
+ * \param Id unique identifier of the process
+ * \param port where the server will listen
+ **********************************/
+public Chat(String alias, int myPort) {
+        System.out.println("Starting Chat Application...");
+        this.alias = alias;
+        this.myPort = myPort;
+        // Initialization of the peer
+        Thread server = new Thread(new Server());
+        Thread client = new Thread(new Client());
+        server.start();
+        client.start();
+        try {
+                //Default thread code, if the thread dies, join back into the main thread
+                client.join();
+                server.join();
+        } catch (InterruptedException e)
+        {
+                // Handle Exception
+                System.out.println("Could Not Join the Client or server back to main thread");
+        }
+}
 
+public static void main(String[] args) {
 
-  /*****************************//*
-   * \brief It implements the client
-   **********************************/
-  private class Client implements Runnable
-  {
+        //Set our debug mode
+        debugMode = true;
 
-    public Client()
-    {
-    }
-
-    /*****************************//**
-     * \brief It allows the user to interact with the system.
-     **********************************/
-    public void run()
-    {
-            while (true)
-            {
-                    /*
-                       Create a simple user interface
-
-                       The first thing to do is to join
-                       ask the ip and port when joining and set ipSuccessor = ip, portSuccessor = port
-                       Socket socket = new Socket(ipSuccessor, portSuccessor);
-
-
-                       // Create the mssages m using JsonWriter and send it as stream
-
-                       ObjectOutputStream oos = new
-                       ObjectOutputStream(socket.getOutputStream());
-                       ObjectInputStream ois = new
-                       ObjectInputStream(socket.getInputStream());
-                       oos.write(m);   this sends the message
-                       ois.read();    reads the response and parse it using JsonParser
-                       socket.close();
-
-                       Use mutex to handle race condition when reading and writing the global variable (ipSuccessor,
-                          portSuccessor, ipPredecessor, portPredecessor)
-
-                     */
-            }
-    }
-  }
-
-  /*****************************//**
-   * Starts the threads with the client and server:
-   * \param Id unique identifier of the process
-   * \param port where the server will listen
-   **********************************/
-  public Chat(String alias, int myPort) {
-
-          this.alias = alias;
-          this.myPort = myPort;
-          // Initialization of the peer
-          Thread server = new Thread(new Server());
-          Thread client = new Thread(new Client());
-          server.start();
-          client.start();
-          try {
-                  client.join();
-                  server.join();
-          } catch (InterruptedException e)
-          {
-                  // Handle Exception
-
-          }
-  }
-
-  public static void main(String[] args) {
-
-          if (args.length < 2 ) {
-                  throw new IllegalArgumentException("Parameter: <alias> <myPort>");
-          }
-          Chat chat = new Chat(args[0], Integer.parseInt(args[1]));
-  }
+        if (args.length < 2 ) {
+                throw new IllegalArgumentException("Parameter: <alias> <myPort>");
+        }
+        Chat chat = new Chat(args[0], Integer.parseInt(args[1]));
+}
 }
---
+++ Distributed Computing - CECS327/assignment2 - distributedchat/README.md
@@ -0,0 +1,5 @@
+# Building and running
+
+Build with `javac -classpath json-simple-1.1.1.jar Chat.java`
+
+Run with `java -cp .:json-simple-1.1.1.jar Chat torch2424 2424`
