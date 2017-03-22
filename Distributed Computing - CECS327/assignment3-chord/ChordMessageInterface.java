62623d636157da4d3aa3ebc5453d6163e3937710
Aaron Turner <aaron@aaronthedev.com>
2017-03-22 09:57:45 -0700

    Finished up the notify function

--- Distributed Computing - CECS327/assignment3-chord/ChordMessageInterface.java
+++ Distributed Computing - CECS327/assignment3-chord/ChordMessageInterface.java
@@ -7,11 +7,11 @@ public interface ChordMessageInterface extends Remote
     ChordMessageInterface locateSuccessor(long key) throws RemoteException;
     ChordMessageInterface closestPrecedingNode(long key) throws RemoteException;
     public void joinRing(String Ip, int port)  throws RemoteException;
-    public void notify(ChordMessageInterface j) throws RemoteException;
+    public void notify(ChordMessageInterface j, boolean isForLeaving) throws RemoteException;
     public boolean isAlive() throws RemoteException;
     public long getId() throws RemoteException;
-    
-    
+
+
     public void put(long guidObject, InputStream file) throws IOException, RemoteException;
     public InputStream get(long guidObject) throws IOException, RemoteException;
     public void delete(long guidObject) throws IOException, RemoteException;
